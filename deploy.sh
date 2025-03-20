b2d1edf1d6c862742b325ec59ee91458fd94dff7
Snehithsk <snehithkrishnamurthi16@gmail.com>
2025-03-20 13:51:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u snehith507 -p dckr_pat_9z52vyGkCDCtQLdgdNOukVIsCOI
-    sudo docker tag test1 snehith507/capstone
-    sudo docker push snehith507/capstone
+     docker tag test1 snehith507/capstone
+     docker push snehith507/capstone
 
     
