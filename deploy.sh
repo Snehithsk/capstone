76a5945cc01cc672978c9ca8138309630170c4cc
Snehithsk <snehithkrishnamurthi16@gmail.com>
2025-03-20 15:11:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u snehith507 -p dckr_pat_9z52vyGkCDCtQLdgdNOukVIsCOI
-     docker tag test1 snehith507/capstone
-     docker push snehith507/capstone
-     docker-compose up -d
+    docker tag test1 snehith507/capstone
+    docker push snehith507/capstone
+    # docker-compose up -d
     
     
